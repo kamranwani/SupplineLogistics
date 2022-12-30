0efcd7bf3cdeea0d7b55bd25202ae95f664a1a76
kamranwani <kamraanwani@gmail.com>
2022-12-30 20:12:38 +0530

    Uncomment code

--- pages/resources.js
+++ pages/resources.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Card from "../components/card";
+
 const Resources = () => {
   const [Data, setData] = useState([
     {
@@ -9,45 +10,45 @@ const Resources = () => {
       paragraph: "Industry Reports",
       footer: "Learn more",
     },
-    // {
-    //   id: 2,
-    //   heading: "Industry Reports2",
-    //   image: "../assets/layer2",
-    //   paragraph: "Container Shipping",
-    //   footer: "Learn more",
-    // },
-    // {
-    //   id: 3,
-    //   heading: "Industry Reports3",
-    //   image: "../assets/layer2",
+    {
+      id: 2,
+      heading: "Industry Reports2",
+      image: "../assets/layer2",
+      paragraph: "Container Shipping",
+      footer: "Learn more",
+    },
+    {
+      id: 3,
+      heading: "Industry Reports3",
+      image: "../assets/layer2",
 
-    //   paragraph: "Port Information",
-    //   footer: "Learn more",
-    // },
-    // {
-    //   id: 4,
-    //   heading: "Industry Reports3",
-    //   image: "../assets/layer2",
-    //   paragraph:
-    //     "As a Transport and logistics company,our job is to get your goods safely",
-    //   footer: "Learn more",
-    // },
-    // {
-    //   id: 5,
-    //   heading: "Industry Reports3",
-    //   image: "../assets/layer2",
-    //   paragraph:
-    //     "As a Transport and logistics company,our job is to get your goods safely",
-    //   footer: "Learn more",
-    // },
-    // {
-    //   id: 6,
-    //   heading: "Industry Reports3",
-    //   image: "../assets/layer2",
-    //   paragraph:
-    //     "As a Transport and logistics company,our job is to get your goods safely",
-    //   footer: "Learn more",
-    // },
+      paragraph: "Port Information",
+      footer: "Learn more",
+    },
+    {
+      id: 4,
+      heading: "Industry Reports3",
+      image: "../assets/layer2",
+      paragraph:
+        "As a Transport and logistics company,our job is to get your goods safely",
+      footer: "Learn more",
+    },
+    {
+      id: 5,
+      heading: "Industry Reports3",
+      image: "../assets/layer2",
+      paragraph:
+        "As a Transport and logistics company,our job is to get your goods safely",
+      footer: "Learn more",
+    },
+    {
+      id: 6,
+      heading: "Industry Reports3",
+      image: "../assets/layer2",
+      paragraph:
+        "As a Transport and logistics company,our job is to get your goods safely",
+      footer: "Learn more",
+    },
   ]);
 
   return (
